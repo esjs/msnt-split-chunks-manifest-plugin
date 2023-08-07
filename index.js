da4597ffb52848fcb4742cd095212910fd4861ee
Vladimir Panov <vladimir.panov@motorsport.com>
2023-08-07 16:40:13 +0300

    Update to work with Webpack5

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { Compilation } = require('webpack');
 
 class MsntSplitChunksManifestPlugin {
   constructor(options) {
@@ -32,137 +33,152 @@ class MsntSplitChunksManifestPlugin {
   apply(compiler) {
     const commonPagesMapping = this.options.commonPagesMapping;
 
-    compiler.hooks.emit.tapAsync(
+    compiler.hooks.thisCompilation.tap(
       'MsntSplitChunksManifestPlugin',
-      (compilation, cb) => {
-        const mapping = {};
-        const mappingLazy = {};
+      (compilation) => {
+        compilation.hooks.processAssets.tapAsync(
+          {
+            name: 'MsntSplitChunksManifestPlugin',
+            stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS,
+          },
+          async (assets, done) => {
+            const mapping = {};
+            const mappingLazy = {};
 
-        for (const entry of compilation.entrypoints) {
-          const [key, value] = entry;
+            for (const entry of compilation.entrypoints) {
+              const [key, value] = entry;
 
-          const isLazy = key.startsWith('lazy__');
+              const isLazy = key.startsWith('lazy__');
 
-          let files = value
-            .getFiles()
-            .filter(file => file.endsWith(`.${this.options.ext}`));
-          
-          if (this.options.ignoreInitialLazyFiles && isLazy) {
-            files = files.slice(0, -1);
-            
-            if (!files.length) continue;
-          };
-          
-          if (isLazy) {
-            mappingLazy[key] = [];
-          } else {
-            mapping[key] = [];
-          }
+              let files = value
+                .getFiles()
+                .filter((file) => file.endsWith(`.${this.options.ext}`));
 
-          files
-            .forEach(file => {
-              if (
-                this.options.ext === 'css' &&
-                this.options.skipEmptyCSSEntries
-              ) {
-                let source = compilation.assets[file].source();
+              if (this.options.ignoreInitialLazyFiles && isLazy) {
+                files = files.slice(0, -1);
 
-                // check whether only content of file is html
-                if (this.options.checkEmptyRegExp) {
-                  source = source.replace(this.options.checkEmptyRegExp, '');
-                }
-                // check whether only content of file is sourceMap
-                if (this.options.checkSourceMapRegExp) {
-                  source = source.replace(this.options.checkSourceMapRegExp, '');
-                }
-
-                // skip empty entry css files
-                if (!source.trim().length) return;
+                if (!files.length) continue;
               }
 
               if (isLazy) {
-                mappingLazy[key].push(this.options.distPath + file);
+                mappingLazy[key] = [];
               } else {
-                mapping[key].push(this.options.distPath + file);
+                mapping[key] = [];
               }
-            });
 
-          if (
-            commonPagesMapping &&
-            commonPagesMapping[key] &&
-            commonPagesMapping[key].length
-          ) {
-            commonPagesMapping[key].forEach(additionalEntry => {
-              mapping[additionalEntry] = mapping[key].slice();
-            });
+              files.forEach((file) => {
+                if (
+                  this.options.ext === 'css' &&
+                  this.options.skipEmptyCSSEntries
+                ) {
+                  let source = compilation.assets[file].source();
+
+                  // check whether only content of file is html
+                  if (this.options.checkEmptyRegExp) {
+                    source = source.replace(this.options.checkEmptyRegExp, '');
+                  }
+                  // check whether only content of file is sourceMap
+                  if (this.options.checkSourceMapRegExp) {
+                    source = source.replace(
+                      this.options.checkSourceMapRegExp,
+                      ''
+                    );
+                  }
+
+                  // skip empty entry css files
+                  if (!source.trim().length) return;
+                }
+
+                if (isLazy) {
+                  mappingLazy[key].push(this.options.distPath + file);
+                } else {
+                  mapping[key].push(this.options.distPath + file);
+                }
+              });
+
+              if (
+                commonPagesMapping &&
+                commonPagesMapping[key] &&
+                commonPagesMapping[key].length
+              ) {
+                commonPagesMapping[key].forEach((additionalEntry) => {
+                  mapping[additionalEntry] = mapping[key].slice();
+                });
+              }
+            }
+
+            const data = JSON.stringify(mapping);
+            const dataLazy = JSON.stringify(mappingLazy);
+
+            compilation.assets[`${this.options.dist}${this.options.name}`] = {
+              source() {
+                return data;
+              },
+              size() {
+                return data.length;
+              },
+            };
+            compilation.assets[`${this.options.dist}${this.options.nameLazy}`] =
+              {
+                source() {
+                  return dataLazy;
+                },
+                size() {
+                  return dataLazy.length;
+                },
+              };
+
+            if (this.options.ext === 'css' && this.options.rtl) {
+              const mappingRtl = Object.assign({}, mapping);
+              for (let i in mappingRtl) {
+                mappingRtl[i] = mappingRtl[i].map(
+                  (file) =>
+                    path.dirname(file) +
+                    '/' +
+                    this.options.rtlDist +
+                    path.basename(file)
+                );
+              }
+
+              const mappingLazyRtl = Object.assign({}, mappingLazy);
+              for (let i in mappingLazyRtl) {
+                mappingLazyRtl[i] = mappingLazyRtl[i].map(
+                  (file) =>
+                    path.dirname(file) +
+                    '/' +
+                    this.options.rtlDist +
+                    path.basename(file)
+                );
+              }
+
+              const dataRtl = JSON.stringify(mappingRtl);
+              const dataLazyRtl = JSON.stringify(mappingLazyRtl);
+
+              compilation.assets[
+                `${this.options.rtlDist}${this.options.name}`
+              ] = {
+                source() {
+                  return dataRtl;
+                },
+                size() {
+                  return dataRtl.length;
+                },
+              };
+              compilation.assets[
+                `${this.options.rtlDist}${this.options.nameLazy}`
+              ] = {
+                source() {
+                  return dataLazyRtl;
+                },
+                size() {
+                  return dataLazyRtl.length;
+                },
+              };
+            }
+
+            done();
           }
-        }
-
-        const data = JSON.stringify(mapping);
-        const dataLazy = JSON.stringify(mappingLazy);
-
-        compilation.assets[`${this.options.dist}${this.options.name}`] = {
-          source() {
-            return data;
-          },
-          size() {
-            return data.length;
-          },
-        };
-        compilation.assets[`${this.options.dist}${this.options.nameLazy}`] = {
-          source() {
-            return dataLazy;
-          },
-          size() {
-            return dataLazy.length;
-          },
-        };
-
-        if (this.options.ext === 'css' && this.options.rtl) {
-          const mappingRtl = Object.assign({}, mapping);
-          for (let i in mappingRtl) {
-            mappingRtl[i] = mappingRtl[i].map(
-              file =>
-                path.dirname(file) +
-                '/' +
-                this.options.rtlDist +
-                path.basename(file)
-            );
-          }
-
-          const mappingLazyRtl = Object.assign({}, mappingLazy);
-          for (let i in mappingLazyRtl) {
-            mappingLazyRtl[i] = mappingLazyRtl[i].map(
-              file =>
-                path.dirname(file) +
-                '/' +
-                this.options.rtlDist +
-                path.basename(file)
-            );
-          }
-
-          const dataRtl = JSON.stringify(mappingRtl);
-          const dataLazyRtl = JSON.stringify(mappingLazyRtl);
-
-          compilation.assets[`${this.options.rtlDist}${this.options.name}`] = {
-            source() {
-              return dataRtl;
-            },
-            size() {
-              return dataRtl.length;
-            },
-          };
-          compilation.assets[`${this.options.rtlDist}${this.options.nameLazy}`] = {
-            source() {
-              return dataLazyRtl;
-            },
-            size() {
-              return dataLazyRtl.length;
-            },
-          };
-        }
-
-        cb();
+        );
       }
     );
   }
